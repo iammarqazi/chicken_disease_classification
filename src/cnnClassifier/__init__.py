4fac2ca461279bab7e660a7a747e2a57c3ec96c4
Ammar Ahmad Qazi <ammarahmadqazi@Ammars-MacBook-Air.local>
2023-10-14 20:17:19 +0530

    data ingestion completed

--- src/cnnClassifier/__init__.py
+++ src/cnnClassifier/__init__.py
@@ -2,12 +2,12 @@ import os
 import sys
 import logging
 
-logging_str = "[%(asctime): %(levelname): %(module): %(message)s]"
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
 log_dir = "logs"
-log_filepath = os.path.join(log_dir, "running_logs.log")
-
+log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
+
 
 logging.basicConfig(
     level= logging.INFO,
